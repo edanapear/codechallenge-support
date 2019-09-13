fc38d204853adc7947f6a7e3859ecf0b6279e184
edanapear <46693415+edanapear@users.noreply.github.com>
2019-09-13 10:40:05 -0400

    AMD Module

--- feedData.js
+++ feedData.js
@@ -1,177 +1,192 @@
-// import uuid from "uuid/v1";
-// assume uuid is already on the page
+define(
+    //The name of this module
+    "feedData",
 
-let uuidCount = 0;
-function uuid () {
-  return uuidCount++;
-}
+    //The array of dependencies
+    [],
 
-const MAX_OLD_ITEMS_COUNT = 5;
-const MAX_GENERATED_FEED_LENGTH = MAX_OLD_ITEMS_COUNT + 3 + 5;
-const GENERATE_INTERVAL = 3000;
-const icons = ["image", "work", "beach"];
+    //The function to execute when all dependencies have loaded. The
+    //arguments to this function are the array of dependencies mentioned
+    //above.
+    function () {
 
-const stubbedData = [
-  {
-    content: "Some Image",
-    icon: "image",
-    author: "Danny T",
-    date: getDate(0),
-    uuid: uuid()
-  },
-  {
-    content: "Some Work",
-    icon: "work",
-    author: "John S",
-    date: getDate(-60000),
-    uuid: uuid()
-  },
-  {
-    content: "Some Beach",
-    icon: "beach",
-    author: "Tyrion L",
-    date: getDate(-360000),
-    uuid: uuid()
-  },
-  ...createOldItems(MAX_OLD_ITEMS_COUNT)
-];
+      // import uuid from "uuid/v1";
+      // assume uuid is already on the page
 
-const feedGenerator = (generateInterval = GENERATE_INTERVAL) => {
-  let data = [...stubbedData];
-  let newItemCallback = () => {};
-  const concatData = items => {
-    data = data.concat(items);
-  };
-  const getData = () => {
-    return data;
-  };
-  const getOlderDataByTimestamp = (timeStamp, count) => {
-    let itemCount = 0;
-    return data
-      .sort((a, b) => b.date - a.date)
-      .filter(item => {
-        if (+item.date <= +timeStamp && itemCount < count) {
-          itemCount++;
-          return true;
-        } else {
-          return false;
-        }
-      });
-  };
-  const getNewerDataByTimestamp = timeStamp => {
-    return data
-      .sort((a, b) => b.date - a.date)
-      .filter(item => {
-        return +item.date >= +timeStamp;
-      });
-  };
-  const startFeedGenerator = () => createNewItemAtInterval();
-  const stopFeedGenerator = intervalProcessId =>
-    clearInterval(intervalProcessId);
-  const subscribeWithCallback = (timeStamp, callback) => {
-    newItemCallback = () => callback(getNewerDataByTimestamp(timeStamp));
-  };
-  const createNewItemAtInterval = () => {
-    const intervalProcessId = setInterval(() => {
-      if (data.length >= MAX_GENERATED_FEED_LENGTH) {
-        stopFeedGenerator(intervalProcessId);
+      let uuidCount = 0;
+      function uuid () {
+        return uuidCount++;
       }
-      concatData(createNewPost());
-      console.log("creating new post");
-      newItemCallback(data);
-    }, generateInterval);
-  };
 
-  const unsubscribe = () => stopFeedGenerator();
+      const MAX_OLD_ITEMS_COUNT = 5;
+      const MAX_GENERATED_FEED_LENGTH = MAX_OLD_ITEMS_COUNT + 3 + 5;
+      const GENERATE_INTERVAL = 3000;
+      const icons = ["image", "work", "beach"];
 
-  return {
-    getData,
-    getNewerDataByTimestamp,
-    getOlderDataByTimestamp,
-    startFeedGenerator,
-    subscribeWithCallback,
-    unsubscribe
-  };
-};
+      const stubbedData = [
+        {
+          content: "Some Image",
+          icon: "image",
+          author: "Danny T",
+          date: getDate(0),
+          uuid: uuid()
+        },
+        {
+          content: "Some Work",
+          icon: "work",
+          author: "John S",
+          date: getDate(-60000),
+          uuid: uuid()
+        },
+        {
+          content: "Some Beach",
+          icon: "beach",
+          author: "Tyrion L",
+          date: getDate(-360000),
+          uuid: uuid()
+        },
+        ...createOldItems(MAX_OLD_ITEMS_COUNT)
+      ];
 
-const pseudoLiveFeed = feedGenerator();
+      const feedGenerator = (generateInterval = GENERATE_INTERVAL) => {
+        let data = [...stubbedData];
+        let newItemCallback = () => {};
+        const concatData = items => {
+          data = data.concat(items);
+        };
+        const getData = () => {
+          return data;
+        };
+        const getOlderDataByTimestamp = (timeStamp, count) => {
+          let itemCount = 0;
+          return data
+            .sort((a, b) => b.date - a.date)
+            .filter(item => {
+              if (+item.date <= +timeStamp && itemCount < count) {
+                itemCount++;
+                return true;
+              } else {
+                return false;
+              }
+            });
+        };
+        const getNewerDataByTimestamp = timeStamp => {
+          return data
+            .sort((a, b) => b.date - a.date)
+            .filter(item => {
+              return +item.date >= +timeStamp;
+            });
+        };
+        const startFeedGenerator = () => createNewItemAtInterval();
+        const stopFeedGenerator = intervalProcessId =>
+          clearInterval(intervalProcessId);
+        const subscribeWithCallback = (timeStamp, callback) => {
+          newItemCallback = () => callback(getNewerDataByTimestamp(timeStamp));
+        };
+        const createNewItemAtInterval = () => {
+          const intervalProcessId = setInterval(() => {
+            if (data.length >= MAX_GENERATED_FEED_LENGTH) {
+              stopFeedGenerator(intervalProcessId);
+            }
+            concatData(createNewPost());
+            console.log("creating new post");
+            newItemCallback(data);
+          }, generateInterval);
+        };
 
-function subscribeToNewFeedPosts(timeStamp, onChangeCallback) {
-  pseudoLiveFeed.subscribeWithCallback(timeStamp, onChangeCallback);
-  return () => pseudoLiveFeed.unsubscribe();
-}
+        const unsubscribe = () => stopFeedGenerator();
 
-function createOldItems(count) {
-  const arrayTemplate = new Array(count).fill({});
-  const oldItems = arrayTemplate.map((item, i) => {
-    const date = getDate(
-      // generate random date ~1-8 days prior
-      -(i * 1000 * 60 * 60 * Math.random() * 24 * 7) - 1000 * 60 * 60 * 24
-    );
-    const icon = getRandomFromList(icons);
-    return {
-      content: `OLD ${icon} post`,
-      icon,
-      date,
-      author: "[autogen]",
-      uuid: uuid()
-    };
-  });
-  return oldItems;
-}
+        return {
+          getData,
+          getNewerDataByTimestamp,
+          getOlderDataByTimestamp,
+          startFeedGenerator,
+          subscribeWithCallback,
+          unsubscribe
+        };
+      };
 
-function createNewPost() {
-  const date = getDate();
-  const icon = getRandomFromList(icons);
-  return {
-    content: `NEW ${icon} post`,
-    icon,
-    date,
-    author: "[autogen]",
-    uuid: uuid()
-  };
-}
+      const pseudoLiveFeed = feedGenerator();
 
-function getDate(seconds = 0) {
-  const date = new Date();
-  return +date + seconds;
-}
-
-function loadFeedPosts(time, postCountLimit) {
-  console.log("postCountLimit", postCountLimit);
-  return new Promise((resolve, reject) =>
-    resolve(pseudoLiveFeed.getOlderDataByTimestamp(time, postCountLimit))
-  );
-}
-
-function getOlderDataByTimestamp(data, timeStamp, count) {
-  let itemCount = 0;
-  return data
-    .sort((a, b) => b.date - a.date)
-    .filter(item => {
-      if (+item.date <= +timeStamp && itemCount < count) {
-        itemCount++;
-        return true;
-      } else {
-        return false;
+      function subscribeToNewFeedPosts(timeStamp, onChangeCallback) {
+        pseudoLiveFeed.subscribeWithCallback(timeStamp, onChangeCallback);
+        return () => pseudoLiveFeed.unsubscribe();
       }
-    });
-}
 
-function getPostsByTime(time, postCountLimit) {
-  return getOlderDataByTimestamp(stubbedData, time, postCountLimit);
-}
+      function createOldItems(count) {
+        const arrayTemplate = new Array(count).fill({});
+        const oldItems = arrayTemplate.map((item, i) => {
+          const date = getDate(
+            // generate random date ~1-8 days prior
+            -(i * 1000 * 60 * 60 * Math.random() * 24 * 7) - 1000 * 60 * 60 * 24
+          );
+          const icon = getRandomFromList(icons);
+          return {
+            content: `OLD ${icon} post`,
+            icon,
+            date,
+            author: "[autogen]",
+            uuid: uuid()
+          };
+        });
+        return oldItems;
+      }
 
-function getRandomFromList(list) {
-  const listLength = list.length;
-  return list[Math.floor(Math.random() * listLength)];
-}
+      function createNewPost() {
+        const date = getDate();
+        const icon = getRandomFromList(icons);
+        return {
+          content: `NEW ${icon} post`,
+          icon,
+          date,
+          author: "[autogen]",
+          uuid: uuid()
+        };
+      }
+
+      function getDate(seconds = 0) {
+        const date = new Date();
+        return +date + seconds;
+      }
+
+      function loadFeedPosts(time, postCountLimit) {
+        console.log("postCountLimit", postCountLimit);
+        return new Promise((resolve, reject) =>
+          resolve(pseudoLiveFeed.getOlderDataByTimestamp(time, postCountLimit))
+        );
+      }
+
+      function getOlderDataByTimestamp(data, timeStamp, count) {
+        let itemCount = 0;
+        return data
+          .sort((a, b) => b.date - a.date)
+          .filter(item => {
+            if (+item.date <= +timeStamp && itemCount < count) {
+              itemCount++;
+              return true;
+            } else {
+              return false;
+            }
+          });
+      }
+
+      function getPostsByTime(time, postCountLimit) {
+        return getOlderDataByTimestamp(stubbedData, time, postCountLimit);
+      }
+
+      function getRandomFromList(list) {
+        const listLength = list.length;
+        return list[Math.floor(Math.random() * listLength)];
+      }
 
 
-// return {
-//   stubbedData,
-//   getPostsByTime,
-//   loadFeedPosts,
-//   pseudoLiveFeed,
-//   subscribeToNewFeedPosts,
-// };
+      return {
+        stubbedData,
+        getPostsByTime,
+        loadFeedPosts,
+        pseudoLiveFeed,
+        subscribeToNewFeedPosts,
+      };
+
+    }
+);
